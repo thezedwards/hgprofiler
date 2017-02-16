9ab20ec69c7650478228d4e327dbca6e7c460d27
Luke Maxwell <lukemaxwellshouse@gmail.com>
2017-02-16 16:42:53 +0000

    Random password function

--- lib/helper/functions.py
+++ lib/helper/functions.py
@@ -20,3 +20,25 @@ def random_string(n):
     rand = random.SystemRandom()
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     return ''.join(rand.choice(chars) for _ in range(n))
+
+
+def random_password():
+    """
+    Generate 12 character password with at least:
+
+     * 1 uppercase character
+     * 1 lower case character
+     * 3 digits
+    """
+    rand = random.SystemRandom()
+    chars = string.ascii_letters + string.digits
+
+    while True:
+        password = ''.join(rand.choice(chars) for i in range(12))
+
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+
+    return password
